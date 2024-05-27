f0155462d771dac4cf5d84784a9d8e5f097a7a3f
RalphCajazeira <ralphmtk@gmail.com>
2024-05-27 14:16:00 -0300

    Codigo da Aula

--- codigo-aula-backend-metodos-arrays-3/video-11-exercicio-resolvido-2/scripts.js
+++ codigo-aula-backend-metodos-arrays-3/video-11-exercicio-resolvido-2/scripts.js
@@ -1,16 +1,16 @@
 const carros = [
-    { nome: "corola", marca: "toyota", ano: "2020", cor: "prata" },
-    { nome: "argo", marca: "fiat", ano: "2021", cor: "preto" },
-    { nome: "ranger", marca: "ford", ano: "2021", cor: "prata" },
-    { nome: "hilux", marca: "toyota", ano: "2018", cor: "branco" },
+  { nome: "corola", marca: "toyota", ano: "2020", cor: "prata" },
+  { nome: "argo", marca: "fiat", ano: "2021", cor: "preto" },
+  { nome: "ranger", marca: "ford", ano: "2021", cor: "prata" },
+  { nome: "hilux", marca: "toyota", ano: "2018", cor: "branco" },
 ];
 
-const ordenarCarros = (arrayCarros, campoOrdenacao) => {
-    arrayCarros.sort((primeiro, segundo) => {
-        return primeiro[campoOrdenacao].localeCompare(segundo[campoOrdenacao]);
-    });
+const campoOrdenacaoPadrao = "nome";
 
-    console.log(arrayCarros);
-}
+const ordenarCarros = (arraycarros, campoOrdenacao) => {
+  return arraycarros.sort((primeiro, segundo) => {
+    return primeiro[campoOrdenacao].localeCompare(segundo[campoOrdenacao]);
+  });
+};
 
-ordenarCarros(carros, 'cor');
+console.log(ordenarCarros(carros, campoOrdenacao));
--- codigo-aula-backend-metodos-arrays-3/video-16-exercicio-resolvido-3/scripts.js
+++ codigo-aula-backend-metodos-arrays-3/video-16-exercicio-resolvido-3/scripts.js
@@ -1,19 +1,22 @@
 const usuarios = [
-    { id: 11, nome: "joao", idade: 23 },
-    { id: 2, nome: "maria", idade: 18 },
-    { id: 4, nome: "ana", idade: 30 },
-    { id: 1, nome: "rodrigo", idade: 17 },
-    { id: 123, nome: "rodrigo", idade: 50 },
+  { id: 11, nome: "joao", idade: 23 },
+  { id: 2, nome: "maria", idade: 18 },
+  { id: 4, nome: "ana", idade: 30 },
+  { id: 1, nome: "rodrigo", idade: 17 },
+  { id: 123, nome: "rodrigo", idade: 50 },
 ];
 
-const maiorIdade = usuarios.reduce((acumulador, elementoAtual) => {
-    let maior = acumulador;
-
-    if (elementoAtual.idade > maior.idade) {
-        maior = elementoAtual;
+const filtroMaiorIdade = usuarios.reduce(
+  (acumulador, elementoAtual, index, array) => {
+    //não precisa deixar o indice e o array, deixei so para saber que tem
+    let resultado = acumulador;
+    if (elementoAtual.idade > acumulador.idade) {
+      resultado = elementoAtual;
     }
+    return resultado;
+  }
+);
 
-    return maior;
-});
-
-console.log(maiorIdade);
+console.log(
+  `Dessa lista o(a)${filtroMaiorIdade.nome} é o mais velho(a) com ${filtroMaiorIdade.idade} de Idade`
+);
